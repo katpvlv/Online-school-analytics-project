590c7b7acea148d51fb2a451283fb7a228eeefb1
Ekaterina <81798251+katpvlv@users.noreply.github.com>
2024-02-08 19:17:15 +0400

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -8,50 +8,47 @@ select count(case when status_id = 142 then visitor_id end)
 from leads;
 
 with tab as (
-	select
-		date(visit_date) as day_,
-		*
-	from sessions
+    select
+        date(visit_date) as day_,
+        *
+    from sessions
 ),
 
 tab2 as (
-	select 
-		count(visitor_id) over (partition by day_, source order by day_) as visitors,
-		row_number() over (partition by day_, source order by day_) as rn,
-		day_,
-		source
-	from tab
+    select
+        count(visitor_id) over (partition by day_, source order by day_) as visitors,
+        row_number() over (partition by day_, source order by day_) as rn,
+        day_,
+        source
+    from tab
 )
 
 select
-	visitors,
-	extract(month from day_) as month_,
-	extract(week from day_) as week,
-	day_,
-	source 
+    visitors,
+    extract(month from day_) as month_,
+    extract(week from day_) as week_,
+    day_,
+    source 
 from tab2
-where rn = 1
-;
+where rn = 1;
 
-select 
-	date(s.visit_date) as date,
-	extract(week from s.visit_date) as week,
-	extract(month from s.visit_date) as month_,
-	count(distinct l.lead_id) as lead,
-	s.source
-from sessions s 
-join leads l 
-	on s.visitor_id = l.visitor_id
-group by 1, 2, 3, 5
-;
+select
+    date(s.visit_date) as date_,
+    extract(week from s.visit_date) as week_,
+    extract(month from s.visit_date) as month_,
+    count(distinct l.lead_id) as lead,
+    s.source
+from sessions s
+left join leads l
+    on s.visitor_id = l.visitor_id
+group by 1, 2, 3, 5;
 
-select 
-	date(created_at),
-	count(case when status_id = 142 then visitor_id end) as purchase
+select
+    date(created_at) as date,
+    count(case when status_id = 142 then visitor_id end) as purchase
 from leads
 group by 1
-order by 1
-;
+order by 1;
 
 with sessions_with_paid_mark as (
     select
@@ -162,4 +159,3 @@ left join marketing_data as m
         and lower(a.utm_medium) = m.utm_medium
         and lower(a.utm_campaign) = m.utm_campaign
 order by 9 desc nulls last, 1, 5 desc, 2, 3, 4;
-
